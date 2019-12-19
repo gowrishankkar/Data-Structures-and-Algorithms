de6c6e350b26dfcdb78e82831b9a90848f7209d4
Gowri Shankar <shankarg.kvsulur@gmail.com>
2019-12-19 10:43:29 +0530

    Recursion

---
+++ Recursion/Factorial.js
@@ -0,0 +1,15 @@
+// Factorial Iterative
+
+function factorial(num) {
+  let total = 1;
+  for (let i = num; i > 1; i--) {
+    total *= i;
+  }
+  return total;
+}
+
+// Factorial Recursive
+function factorial(num) {
+  if (num === 1) return 1;
+  return num * factorial(num - 1);
+}
