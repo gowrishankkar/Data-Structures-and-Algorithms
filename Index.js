c51be5a7fdefff6874859331df282f3299b53272
gowrishankkar <shankarg.kvsulur@gmail.com>
2021-05-19 23:42:44 +0530

    BST insert and lookup

--- Index.js
+++ Index.js
@@ -1,46 +1,77 @@
-class Node{
-    constructor(value){
-        this.left = null;
-        this.right = null;
-        this.value = value;
-    }
+class Node {
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
 }
 
 class BinarySearchTree {
-    constructor(){
-        this.root = null;
-    }
+  constructor() {
+    this.root = null;
+  }
 
-    insert(value){
-       
-
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (value < currentNode.value) {
+          //    left
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          // right
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
     }
+  }
 
-    lookup(value){
-        
+  lookup(value) {
+    if (!this.root) {
+      return false;
     }
+    let currentNode = this.root;
+    while (currentNode) {
+      if (value < currentNode.value) {
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        currentNode = currentNode.right;
+      } else if (value === currentNode.value) {
+        return currentNode;
+      }
+      return false;
+    }
+  }
 
-    remove(value){
-       
-    }
+  remove(value) {}
 }
 
 const tree = new BinarySearchTree();
 tree.insert(10);
-// tree.insert(10);
-// tree.insert(10);
-// tree.insert(10);
+tree.insert(5);
+tree.insert(1);
+tree.insert(17);
 // tree.insert(10);
 // tree.insert(10);
 // tree.insert(10);
 // tree.insert(10);
 // tree.lookup(10);
-JSON.stringify(traverse(tree.root))
-console.log('tree', JSON.stringify(traverse(tree.root)))
+JSON.stringify(traverse(tree.root));
+console.log("tree", tree);
 function traverse(node) {
-    const tree = { value: node.value };
-    tree.left = node.left === null ? null : traverse(node.left);
-    tree.right = node.right === null ? null : traverse(node.right);
-    return tree;
-  }
-  
+  const tree = { value: node.value };
+  tree.left = node.left === null ? null : traverse(node.left);
+  tree.right = node.right === null ? null : traverse(node.right);
+  return tree;
+}
