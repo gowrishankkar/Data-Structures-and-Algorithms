2b282719fdc3a5a0a9c13c132fa09cc7dd90ff16
gowrishankkar <shankarg.kvsulur@gmail.com>
2021-05-20 11:29:57 +0530

    Testing Fibonacci

--- Index.js
+++ Index.js
@@ -1,77 +1,20 @@
-class Node {
-  constructor(value) {
-    this.left = null;
-    this.right = null;
-    this.value = value;
+function fibIterative(num) {
+  let arr = [0, 1];
+  for (let i = 2; i < num; i++) {
+    arr.push(arr[i - 2] + arr[i - 1]);
   }
+
+  return arr[n];
 }
 
-class BinarySearchTree {
-  constructor() {
-    this.root = null;
-  }
+fibIterative(10);
 
-  insert(value) {
-    const newNode = new Node(value);
-    if (this.root === null) {
-      this.root = newNode;
-    } else {
-      let currentNode = this.root;
-      while (true) {
-        if (value < currentNode.value) {
-          //    left
-          if (!currentNode.left) {
-            currentNode.left = newNode;
-            return this;
-          }
-          currentNode = currentNode.left;
-        } else {
-          // right
-          if (!currentNode.right) {
-            currentNode.right = newNode;
-            return this;
-          }
-          currentNode = currentNode.right;
-        }
-      }
-    }
+function fibRecursive(num) {
+  if (num < 2) {
+    return num;
   }
-
-  lookup(value) {
-    if (!this.root) {
-      return false;
-    }
-    let currentNode = this.root;
-    while (currentNode) {
-      if (value < currentNode.value) {
-        currentNode = currentNode.left;
-      } else if (value > currentNode.value) {
-        currentNode = currentNode.right;
-      } else if (value === currentNode.value) {
-        return currentNode;
-      }
-      return false;
-    }
-  }
-
-  remove(value) {}
+  console.log(num);
+  return fibRecursive(num - 1) + fibRecursive(num - 2);
 }
 
-const tree = new BinarySearchTree();
-tree.insert(10);
-tree.insert(5);
-tree.insert(1);
-tree.insert(17);
-// tree.insert(10);
-// tree.insert(10);
-// tree.insert(10);
-// tree.insert(10);
-// tree.lookup(10);
-JSON.stringify(traverse(tree.root));
-console.log("tree", tree);
-function traverse(node) {
-  const tree = { value: node.value };
-  tree.left = node.left === null ? null : traverse(node.left);
-  tree.right = node.right === null ? null : traverse(node.right);
-  return tree;
-}
+console.log(fibRecursive(10));
---
+++ Recursion/fibonacci.js
@@ -0,0 +1,21 @@
+function fibIterative(num) {
+    let arr = [0, 1];
+    for (let i = 2; i < num; i++) {
+      arr.push(arr[i - 2] + arr[i - 1]);
+    }
+  
+    return arr[n];
+  }
+  
+  fibIterative(10);
+  
+  function fibRecursive(num) {
+    if (num < 2) {
+      return num;
+    }
+    console.log(num);
+    return fibRecursive(num - 1) + fibRecursive(num - 2);
+  }
+  
+  console.log(fibRecursive(10));
+  
