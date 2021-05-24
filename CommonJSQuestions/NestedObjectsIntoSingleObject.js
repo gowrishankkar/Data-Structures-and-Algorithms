07e1bd9387b973b437ca43964709249a378291fa
gowrishankkar <shankarg.kvsulur@gmail.com>
2021-05-24 09:23:15 +0530

    Breadth First Search

--- CommonJSQuestions/NestedObjectsIntoSingleObject.js
+++ CommonJSQuestions/NestedObjectsIntoSingleObject.js
@@ -1,32 +1,35 @@
 let user = {
-    name: "Test Name",
-    address: {
-      personal: {
-        city: "cbe",
-        area: "sullur",
-      },
-      office: {
-        city: "cbe 2",
-        area: "hopes",
-      },
+  name: "Test Name",
+  address: {
+    personal: {
+      city: "cbe",
+      area: "sullur",
     },
-  };
-  
-  let finalObject = {};
-  let showAll = (obj, parent) => {
-    for (let key in obj) {
-      let keyName = parent+"_"+key
-      if (typeof obj[key] === 'object') {
-  
-        showAll(obj[key], keyName);
-      } else {
-      
-        console.log(keyName)
-        finalObject[keyName] = obj[key]
-      }
+    office: {
+      city: "cbe 2",
+      area: "hopes",
+    },
+  },
+};
+
+let finalObject = {};
+let showAll = (obj, parent) => {
+  for (let key in obj) {
+    let keyName = parent + "_" + key;
+    if (typeof obj[key] === "object") {
+      showAll(obj[key], keyName);
+    } else {
+      console.log(keyName);
+      finalObject[keyName] = obj[key];
     }
-  };
-  
-  showAll(user, 'user');
-  console.log(finalObject)
-  
+  }
+};
+
+showAll(user, "user");
+console.log(finalObject);
+
+// {user_address_office_area: "hopes"
+// user_address_office_city: "cbe 2"
+// user_address_personal_area: "sullur"
+// user_address_personal_city: "cbe"
+// user_name: "Test Name"}
