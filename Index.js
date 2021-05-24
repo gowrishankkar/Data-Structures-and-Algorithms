343d44ef43267ba44a440f5f67f0fe2ead142a27
gowrishankkar <shankarg.kvsulur@gmail.com>
2021-05-24 09:29:07 +0530

    Breadth First Search Recursive

--- Index.js
+++ Index.js
@@ -146,6 +146,21 @@ class Node {
       }
       return list;
     }
+
+    breadthFirstSearchRecursive(queue, list){
+        if(!queue.length){
+            return list;
+        }
+       let  currentNode = queue.shift();
+        list.push(currentNode.value);
+        if (currentNode.left) {
+          queue.push(currentNode.left);
+        } 
+        if (currentNode.right) {
+          queue.push(currentNode.right);
+        }
+        return this.breadthFirstSearchRecursive(queue, list)
+    }
   }
   
   const tree = new BinarySearchTree();
@@ -157,7 +172,7 @@ class Node {
   tree.insert(15);
   tree.insert(1);
   
-  console.log("BFS", tree.breadthFirstSearch());
+  console.log("BFS", tree.breadthFirstSearchRecursive([tree.root], []));
   
   //     9
   //  4     20
